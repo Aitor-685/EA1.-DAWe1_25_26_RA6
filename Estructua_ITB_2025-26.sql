265d6c29cf335691ebd30ba69090d67f8986b22c
Aitor <aitor.obeso.7e6@itb.cat>
2025-11-24 10:00:17 +0100

    Ultimos cambios

--- Estructua_ITB_2025-26.sql
+++ Estructua_ITB_2025-26.sql
@@ -1,26 +1,26 @@
 -- Crear base de datos
 CREATE DATABASE itb;
 
--- Crear usuari
+-- Crear usuario
 CREATE USER usuarioitb WITH SUPERUSER CREATEROLE ENCRYPTED PASSWORD 'usuarioitb';
 
--- Asignar base de datos al usuari
+-- Asignar propietario
 ALTER DATABASE itb OWNER TO usuarioitb;
 
--- Darle todos los permisos ha ese usuario
+-- Permisos
 GRANT ALL PRIVILEGES ON DATABASE itb TO usuarioitb;
 
--- Estructura de base de datos
 CREATE TABLE Grup (
     Nom VARCHAR(5) NOT NULL,
     Aula NUMERIC(3) NOT NULL,
-    CONSTRAINT PK_grup PRIMARY KEY (Nom)
+    CONSTRAINT pk_grup PRIMARY KEY (Nom),
+    CONSTRAINT ck_aula CHECK (Aula > 0)
 );
 
 CREATE TABLE Modul (
     Codi VARCHAR(10) NOT NULL,
     Nom VARCHAR(60) NOT NULL,
-    CONSTRAINT PK_modul PRIMARY KEY (Codi)
+    CONSTRAINT pk_modul PRIMARY KEY (Codi)
 );
 
 CREATE TABLE Docent (
@@ -29,7 +29,9 @@ CREATE TABLE Docent (
     Cognom1 VARCHAR(10) NOT NULL,
     Email_itb VARCHAR(40) NOT NULL,
     Experiencia NUMERIC(2) NOT NULL,
-    CONSTRAINT PK_docent PRIMARY KEY (Codi)
+    CONSTRAINT pk_docent PRIMARY KEY (Codi),
+    CONSTRAINT ck_experiencia CHECK (Experiencia >= 0),
+    CONSTRAINT uq_docent_email UNIQUE (Email_itb)
 );
 
 CREATE TABLE Alumne (
@@ -41,48 +43,50 @@ CREATE TABLE Alumne (
     Telefon NUMERIC(9),
     Codi_postal NUMERIC(5) NOT NULL,
     Grup VARCHAR(5) NOT NULL,
-    CONSTRAINT PK_Alumne PRIMARY KEY (Codi),
-    CONSTRAINT FK_grup FOREIGN KEY (Grup) REFERENCES Grup(Nom)
+    CONSTRAINT pk_alumne PRIMARY KEY (Codi),
+    CONSTRAINT uq_alumne_email UNIQUE (Email_itb),
+    CONSTRAINT fk_alumne_grup FOREIGN KEY (Grup) REFERENCES Grup(Nom)
 );
 
 CREATE TABLE Ensenya (
-    Docent NUMERIC(3) NOT NULL,
-    Modul VARCHAR(10) NOT NULL,
+    Codi_docent NUMERIC(3) NOT NULL,
+    Codi_modul VARCHAR(10) NOT NULL,
     Grup VARCHAR(5) NOT NULL,
-    CONSTRAINT PK_Ensenya PRIMARY KEY (Docent, Modul, Grup),
-    CONSTRAINT FK_docent FOREIGN KEY (Docent) REFERENCES Docent(Codi),
-    CONSTRAINT FK_modul FOREIGN KEY (Grup) REFERENCES Modul(Codi),
-    CONSTRAINT FK_grups FOREIGN KEY (Grup) REFERENCES Grup(Nom)
+    CONSTRAINT pk_ensenya PRIMARY KEY (Codi_docent, Codi_modul, Grup),
+    CONSTRAINT fk_ensenya_docent FOREIGN KEY (Codi_docent) REFERENCES Docent(Codi),
+    CONSTRAINT fk_ensenya_modul FOREIGN KEY (Codi_modul) REFERENCES Modul(Codi),
+    CONSTRAINT fk_ensenya_grup FOREIGN KEY (Grup) REFERENCES Grup(Nom)
 );
 
 CREATE TABLE Activitat (
     Codi VARCHAR(5) NOT NULL,
     Nom VARCHAR(20) NOT NULL,
     Descripcio VARCHAR(20),
-    CONSTRAINT PK_activitat PRIMARY KEY (Codi)
+    CONSTRAINT pk_activitat PRIMARY KEY (Codi)
 );
 
 CREATE TABLE Preferencia (
     Codi VARCHAR(5) NOT NULL,
     Nom VARCHAR(20) NOT NULL,
     Descripcio VARCHAR(20),
-    CONSTRAINT PK_preferencia PRIMARY KEY (Codi)
+    CONSTRAINT pk_preferencia PRIMARY KEY (Codi)
 );
 
 CREATE TABLE Realitza (
     Codi_Al NUMERIC(2) NOT NULL,
     Codi_Act VARCHAR(5) NOT NULL,
-    Hores_Sem VARCHAR(5) NOT NULL,
-    CONSTRAINT PK_realitza PRIMARY KEY (Codi_Al, Codi_Act),
-    CONSTRAINT FK_alumnes1 FOREIGN KEY (Codi_Al) REFERENCES Alumne(Codi),
-    CONSTRAINT FK_activitats1 FOREIGN KEY (Grup) REFERENCES Activitat(Codi)
+    Hores NUMERIC(3) NOT NULL,
+    CONSTRAINT pk_realitza PRIMARY KEY (Codi_Al, Codi_Act),
+    CONSTRAINT fk_realitza_alumne FOREIGN KEY (Codi_Al) REFERENCES Alumne(Codi),
+    CONSTRAINT fk_realitza_activitat FOREIGN KEY (Codi_Act) REFERENCES Activitat(Codi),
+    CONSTRAINT ck_realitza_hores CHECK (Hores >= 0)
 );
 
 CREATE TABLE Agrada (
     Codi_Al NUMERIC(2) NOT NULL,
     Codi_Pref VARCHAR(5) NOT NULL,
-    CONSTRAINT PK_realitza PRIMARY KEY (Codi_Al, Codi_Pref),
-    CONSTRAINT FK_alumnes2 FOREIGN KEY (Codi_Al) REFERENCES Alumne(Codi),
-    CONSTRAINT FK_activitats1 FOREIGN KEY (Codi_Pref) REFERENCES Preferencia(Codi)
+    CONSTRAINT pk_agrada PRIMARY KEY (Codi_Al, Codi_Pref),
+    CONSTRAINT fk_agrada_alumne FOREIGN KEY (Codi_Al)  REFERENCES Alumne(Codi),
+    CONSTRAINT fk_agrada_preferencia FOREIGN KEY (Codi_Pref) REFERENCES Preferencia(Codi)
 );
 
